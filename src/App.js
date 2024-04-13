c45024b8ecad3df95ea973152456329eb35db64a
johnmungcal2 <johnmungcal2@gmail.com>
2024-04-13 19:53:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      <div className="Main-container">
+        <div className="flex-container">
+          <div className="flex-container-left">
+            <div className="item-1">1</div>
+            <div className="item-2">2</div>
+          </div>
+          <div className="flex-container-right">
+            <div className="item-3">3</div>
+            <div className="item-4">4</div>
+          </div>
+        </div>
+        <div className="grid-container">
+          <div className="item-5">5</div>
+          <div className="item-6">6</div>
+          <div className="item-7">7</div>
+          <div className="item-8">8</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
